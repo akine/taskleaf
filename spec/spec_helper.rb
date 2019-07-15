9693582cadf46542e2f8f0081a5328ecce5209d6
akine <weezer.and.you@gmail.com>
2019-07-15 23:02:10 +0900

    settings for capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,16 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# RSpecでCapybaraを扱うために必要な機能の読み込み
+require 'capybara/rspec'
+
 RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
+
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
